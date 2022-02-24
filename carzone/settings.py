83f10b186d4e8acb1a3ea25094b1807a9386d5f2
Mujtaba Haider <mujtaba.haider89@yahoo.com>
2022-02-24 16:18:01 +0500

    newm

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'aridian890',
+        'HOST':'localhost',
+        'PORT': '',
     }
 }
 
